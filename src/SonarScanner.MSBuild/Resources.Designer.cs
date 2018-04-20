1278c215ecc7f62f58bd1730b9604626bc3933f5
duncanp <duncan.pocklington@sonarsource.com>
2018-04-20 14:34:40 +0100

    Update help text to use new exe name

--- src/SonarScanner.MSBuild/Resources.Designer.cs
+++ src/SonarScanner.MSBuild/Resources.Designer.cs
@@ -133,7 +133,7 @@ namespace SonarScanner.MSBuild {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The C# plugin installed on the SonarQube server is not compatible with the SonarQube analysis agent (i.e. the MSBuild.SonarQube.Scanner.exe, or the build automation task). Either check the compatibility matrix or get the latest versions for both..
+        ///   Looks up a localized string similar to The C# plugin installed on the SonarQube server is not compatible with the SonarQube analysis agent (i.e. the SonarScanner.MSBuild.exe, or the build automation task). Either check the compatibility matrix or get the latest versions for both..
         /// </summary>
         internal static string ERROR_VersionMismatch {
             get {
